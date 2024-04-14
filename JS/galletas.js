5d2080dd4ae52e0a6c0a6f9bcde1fa227b29bf26
Nin0S2341 <166895758+Nin0S2341@users.noreply.github.com>
2024-04-14 04:48:52 +0000

    version 0.3

---
+++ JS/galletas.js
@@ -0,0 +1,10 @@
+const crearcookies=()=>{
+    document.cookie = 'nombre=Jesus; expired= Dec 2024 01:00';
+}
+const iniciarsesion=()=>{
+    const usuario=
+    document.cookie = `nombre=${usuario}; expired= Dec 2024 01:00`;
+    
+}
+
+console.log(document.cookie);
---
+++ JS/registro.js
@@ -0,0 +1,4 @@
+function openWin() {
+    window.open("http://127.0.0.1:5500/dise.html");
+    
+    }
